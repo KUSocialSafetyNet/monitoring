1134b3ed84f7ce78fc0655a1111bbc860541547e
SergioXL55 <sergioxl555@mail.ru>
2018-08-14 09:27:55 +0600

    trst

---
+++ src/main/java/com/my/ftpclient/configuration/FolderConfig.java
@@ -0,0 +1,29 @@
+package com.my.ftpclient.configuration;
+
+/**
+ * Created by User on 13.08.2018.
+ */
+public class FolderConfig {
+
+    private String path;
+    private int period;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public int getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(int period) {
+        this.period = period;
+    }
+
+
+
+}
