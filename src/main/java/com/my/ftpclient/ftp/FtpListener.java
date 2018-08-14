2b9efcd40efbcda72d7c32702ca058113544f16b
SergioXL55 <sergioxl555@mail.ru>
2018-08-14 11:31:49 +0600

    Добавлена еще одна пустая строка

--- src/main/java/com/my/ftpclient/ftp/FtpListener.java
+++ src/main/java/com/my/ftpclient/ftp/FtpListener.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 public interface FtpListener {
 
 
+
     void connectToFtp() throws Exception;
     void disconnect() throws Exception;
     ArrayList<HashMap> getFileList();
